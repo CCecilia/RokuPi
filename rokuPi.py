a72e80bfa55f6375bb485beaa95bd5a837a22c06
Christian Cecilia <chritsian.cecilia1@gmail.com>
2019-10-28 09:28:23 -0400

    added channel  class

--- rokuPi.py
+++ rokuPi.py
@@ -2,14 +2,54 @@
 # -*- coding: utf-8 -*-
 
 import click
+import os
+
+STANDARD_CHANNEL_STRUCTURE = [
+    'manifest',
+    'components/**/*.*',
+    'source/**/*.*',
+    'images/**/*.*'
+]
+
+
+class Channel:
+    def __init__(self, path_to_channel):
+        self.channel_path = path_to_channel
+        self.channel_contents = os.listdir(self.path)
+        self.config_file = self.get_config()
+
+    def get_config(self):
+        file_query = [item for item in self.channel_contents if item == 'rokuPiConfig.json']
+        if len(file_query) == 0:
+            return None
+        else:
+            return file_query[0]
+
+
 
 @click.group()
-def deploy():
+def channel():
     pass
 
-@deploy.command()
-def zipDir():
-    click.echo("Zip Dir")
+@channel.command()
+@click.option('--channel-path', help='path to channel root dir')
+def deploy(channel_path):
+    print(channel_path)
+    current_channel = Channel(channel_path)
+    print(current_channel)
+    if current_channel.config_file == None:
+    #   config creation route
+        create_config = click.prompt('Would like to create config file y/n ', type=str, default='y')
+        if create_config:
+        #
+        else:
+            # use default channel Structure for channel class
+            pass
+    else:
+#         update chanel class from config
+        pass
+
+
 
 
 if __name__ == '__main__':
--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ setup(
     ],
     entry_points='''
         [console_scripts]
-        rokuPi=rokuPi:deploy
+        rokuPi=rokuPi:channel
     ''',
 )
